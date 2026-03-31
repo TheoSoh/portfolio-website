0578785c45e3af994fcfd3cbd51d21ae28fbe188
Theo Sohlman <theo.sohlman@gmail.com>
2026-03-31 15:14:46 +0200

    Add padding to paragraph in Hero section for improved readability

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
     <section className="bg-transparent flex h-[60vh] w-full flex-col md:sticky md:top-[15vh] md:h-[70vh]">
       <h1 className="pb-4 text-6xl font-bold md:text-4xl lg:text-6xl">Theo Sohlman</h1>
       <h2 className="pb-8 text-lg font-semibold">Software Dev.</h2>
-      <p className="text-justify md:text-left 2xl:w-2/3">
+      <p className="text-justify md:text-left 2xl:w-2/3 pr-12">
         Eager to apply knowledge and experience to deliver value and innovation. Explore my work and
         discover how I can contribute to your next project.
       </p>
